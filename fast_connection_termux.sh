fe89639c41145f8225c9d9c425f04d73c968e252
Access1277 <108107724+Access1277@users.noreply.github.com>
2023-07-30 16:34:06 +0800

    Update and rename Updater to fast_connection_termux.sh

---
+++ fast_connection_termux.sh
@@ -0,0 +1,22 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+# Check if the script is invoked with the correct arguments
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <HOST_OR_IP>"
+    exit 1
+fi
+
+HOST_OR_IP="$1"
+
+# Function to test the connection
+test_connection() {
+    local response_time=$(ping -c 5 -i 0.2 -q "$HOST_OR_IP" | grep -oP '(?<=time=)\d+\.\d+')
+    echo "Average response time to $HOST_OR_IP: ${response_time} ms"
+}
+
+# Run the test_connection function repeatedly
+while true; do
+    test_connection
+    sleep 5
+done
+
